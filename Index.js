4196b7afbd061ad49564ec6a5ddb9d36e629b465
Rajan Kumar <129852873+rajan1220@users.noreply.github.com>
2024-04-27 11:27:06 +0530

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,8 @@
+function subscribe() {
+    var txt;
+    if (confirm("Thanks For subscribed  You recived email regarding this wesite!")) {
+      txt = "Done👍👍👍👍👍";
+    } else {
+      txt = " Cancel subscribe";
+    }
+};
